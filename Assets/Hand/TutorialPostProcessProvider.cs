bf0ff49129519a4af57392a3d770c4f74a4e3ae0
DESKTOP-BDVHH83\syslab_macpro <rxia620@gmail.com>
2023-11-28 15:38:08 +0900

    erased the unnecessary parts

--- Assets/Hand/TutorialPostProcessProvider.cs
+++ Assets/Hand/TutorialPostProcessProvider.cs
@@ -86,32 +86,11 @@ public class TutorialPostProcessProvider : MonoBehaviour
         Bone _ringDistal = _ring.Bone(Bone.BoneType.TYPE_DISTAL);
         Bone _pinkyDistal = _pinky.Bone(Bone.BoneType.TYPE_DISTAL);
 
-        /*
-        //Use the _indexFinger to access the bone array, then get the Metacarpal bone from it using the BoneType Enum cast to an int
-        _thumbMetacarpal = _thumb.bones[(int)Bone.BoneType.TYPE_METACARPAL];
-        _indexMetacarpal = _index.bones[(int)Bone.BoneType.TYPE_METACARPAL];
-        _middleMetacarpal = _middle.bones[(int)Bone.BoneType.TYPE_METACARPAL];
-        _ringMetacarpal = _ring.bones[(int)Bone.BoneType.TYPE_METACARPAL];
-        _pinkyMetacarpal = _pinky.bones[(int)Bone.BoneType.TYPE_METACARPAL];
-
-        //Use the _indexFinger to access the bone array, then get the metacarpal by index
-        LEAP_BONE _indexMetacarpal = _index.bones[0];
-        LEAP_BONE _indexProximal = _index.bones[1];
-        LEAP_BONE _indexIntermediate = _index.bones[2];
-        LEAP_BONE _indexDistal = _index.bones[3];
-        */
-
-        Bone _indexDistal1 = _index.bones[3];
-
         LeapTransform _basis = _indexDistal.Basis;
         Vector3 xBasis = _basis.xBasis;
         string xBasisString = xBasis.ToString();
 
-        LeapTransform _basis1 = _indexDistal1.Basis;
-        Vector3 xBasis1 = _basis1.xBasis;
-        string xBasisString1 = xBasis1.ToString();
-
-        string _info = "WhichHand:" + _WhichHand + ", Hand ID: " + _hand.Id + ", Palm Position: " + _hand.PalmPosition + ", xBasis" + xBasisString + ", IndexDistal Direction" + _indexDistal.Direction + ", xBasis1" + xBasisString1 + ", IndexDistal1 Direction" + _indexDistal1.Direction + "\n";
+        string _info = "WhichHand:" + _WhichHand + ", Hand ID: " + _hand.Id +  ", xBasis" + xBasisString + ", IndexDistal Direction" + _indexDistal.Direction + "\n";
 
         return _info;
     }
